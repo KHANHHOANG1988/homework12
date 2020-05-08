237fa5c1a050772d18ed923ce65dced7e79e2d6d
KhanhHoang <Khanhsingapore@gmail.com>
2020-05-07 21:23:46 -0500

    ap.put added

--- server.js
+++ server.js
@@ -20,13 +20,13 @@ app.get("/", (req,res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
   });
   
-  app.get("/exercise", (req,res) => {
-      res.sendFile(path.join(__dirname, 'public', 'exercise.html'));
-  });
+app.get("/exercise", (req,res) => {
+    res.sendFile(path.join(__dirname, 'public', 'exercise.html'));
+});
   
-  app.get("/stats", (req,res) => {
+app.get("/stats", (req,res) => {
     res.sendFile(path.join(__dirname, 'public', 'stats.html'));
-  });
+});
 
   //GET
 app.get("/api/workouts", (req,res) => {
@@ -38,8 +38,8 @@ app.get("/api/workouts", (req,res) => {
       res.json(err);
     });
   });
-  
-  app.get("/api/workouts/range", (req,res) => {
+
+app.get("/api/workouts/range", (req,res) => {
     db.Workout.find({})
     .then(dbWorkout => {
       res.json(dbWorkout);
@@ -49,7 +49,32 @@ app.get("/api/workouts", (req,res) => {
     });
   });
 
+  //PUT
 
+app.put("/api/workouts/:id", (req,res) => {
+
+  let urlData = req.params;
+  let data = req.body;
+    db.Workout.updateOne( {_id: urlData.id }, {$push: {exercises:  [
+      {
+      "type" : data.type,
+      "name" : data.name,
+      "duration" : data.duration,
+      "distance" : data.distance,
+      "weight" : data.weight,
+      "reps" : data.reps,
+      "sets" : data.sets
+      }
+    ] 
+  }}).then(dbUpdate => {
+    res.json(dbUpdate);
+  })
+  .catch(err => {
+    res.json(err);
+  });
+  
+  });
+  
   //POST
 app.post("/api/workouts", (req,res) => {
 
